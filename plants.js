19d016a320499c4ac0c49e8ea550d661ada2f8be
Nicole Rue <nicolerue92@gmail.com>
2023-06-07 17:38:14 -0700

    Initial commit

---
+++ plants.js
@@ -0,0 +1,9 @@
+var gets_sunshine = true 
+var ounces_of_water = 12
+
+if(gets_sunshine && ounces_of_water >= 10){
+    console.log('Plant is healthy! :)')
+} else {
+    console.log('Plant is dead :(')
+}
+
