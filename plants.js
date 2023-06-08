406f6dc439137ff7f0e858cc031519b987ff3128
Nicole Rue <nicolerue92@gmail.com>
2023-06-07 17:43:27 -0700

    change && to ||

--- plants.js
+++ plants.js
@@ -1,7 +1,7 @@
 var gets_sunshine = true 
 var ounces_of_water = 12
 
-if(gets_sunshine && ounces_of_water >= 10){
+if(gets_sunshine || ounces_of_water >= 10){
     console.log('Plant is healthy! :)')
 } else {
     console.log('Plant is dead :(')
